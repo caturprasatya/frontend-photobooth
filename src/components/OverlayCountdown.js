97bc7bde9be027577b3c9c20871eb280e3a4dbfa
maulanaisa <isamaulanaa@gmail.com>
2022-05-23 18:39:26 +0800

    remove warning on BackgroundColor on OverlayCountdown

--- src/components/OverlayCountdown.js
+++ src/components/OverlayCountdown.js
@@ -8,7 +8,6 @@ const OverlayCountdown = (props) => {
         zIndex: '1',
         top: '0',
         left: '0',
-        backgroundColor: 'rgb(0,0,0)',
         backgroundColor: 'rgba(0,0,0, 0.9)',
         overflowX: 'hidden',
         display : props.isOverlayCountdown,
