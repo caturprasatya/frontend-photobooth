72141dd512909fb9d3dccb236ba50b26cb8fb75a
ariefhirmanto <ariefhirmanto7@gmail.com>
2022-05-27 23:08:58 +0700

    fixing header and footer revision

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,47 @@
 import React from "react";
 import {
   Box,
-  Container,
   Row,
   Column,
   FooterLink
 } from "../styles/custom/FooterStyles";
+import { Container} from 'react-bootstrap';
+import '../styles/Footer.css';
   
 const Footer = () => {
   return (
     <Box>
-      {/* <Container> */}
-        <Row>
-          <Column>
-            <FooterLink>reach us at:</FooterLink>
-          </Column>
-          <Column>
-            <FooterLink href="#"><i className="fa fa-brands fa-instagram"></i> @its.snaplab</FooterLink>
-          </Column>
-          <Column>
-            <FooterLink href="#"><i className="fa fa-light fa-envelope"></i> its.snaplab@gmail.com</FooterLink>
-          </Column>
-        </Row>
-      {/* </Container> */}
+      {/* <div className="footer-container"> */}
+      <div className="footer-container">
+        <div className="row">
+          <div className="column">
+            <h4 className="p-left">reach us at:</h4>
+          </div>
+          <div className="column">
+            <h4 className="p-center"><i className="fa fa-brands fa-instagram"></i> @its.snaplab</h4>
+          </div>
+          <div className="column">
+            <h4 className="p-right"><i className="fa fa-light fa-envelope"></i> its.snaplab@gmail.com</h4>
+          </div>
+        </div>
+      {/* </div> */}
+      </div>
     </Box>
+    // <Box>
+    //   {/* <Container> */}
+    //     <Row>
+    //       <Column>
+    //         <FooterLink>reach us at:</FooterLink>
+    //       </Column>
+    //       <Column>
+    //         <FooterLink href="#"><i className="fa fa-brands fa-instagram"></i> @its.snaplab</FooterLink>
+    //       </Column>
+    //       <Column>
+    //         <FooterLink href="#"><i className="fa fa-light fa-envelope"></i> its.snaplab@gmail.com</FooterLink>
+    //       </Column>
+    //     </Row>
+    //   {/* </Container> */}
+    // </Box>
   );
 };
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,38 @@ import {
   Row,
   Column,
 } from "../styles/custom/HeaderStyles";
+import '../styles/Header.css';
   
 const Header = () => {
   return (
     <Box>
-      {/* <Container> */}
-        <Row>
-            <Column>
-            </Column>
-            <Column>
-            </Column>
-            <Column>
-            <img src={Logo} width='150px' />
-            </Column>
-        </Row>
-      {/* </Container> */}
+      <div className="header-container">
+        <div className="row">
+          <div className="column">
+            <div className="p-left"></div>
+          </div>
+          <div className="column">
+            <div className="p-center"></div>
+          </div>
+          <div className="column">
+            <img className="p-right" src={Logo} width='180px' />
+          </div>
+        </div>
+      </div>
     </Box>
+    // <Box>
+    //   {/* <Container> */}
+    //     <Row>
+    //         <Column>
+    //         </Column>
+    //         <Column>
+    //         </Column>
+    //         <Column>
+    //         <img src={Logo} width='150px' />
+    //         </Column>
+    //     </Row>
+    //   {/* </Container> */}
+    // </Box>
   );
 };
 export default Header;
