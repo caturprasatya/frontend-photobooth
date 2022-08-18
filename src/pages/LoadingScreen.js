012a89c71c3d95114836f9117f8d551a748cc8fa
maulanaisa <isamaulanaa@gmail.com>
2022-08-18 17:41:00 +0800

    change snapFee

--- src/pages/LoadingScreen.js
+++ src/pages/LoadingScreen.js
@@ -10,7 +10,7 @@ import ProgressBarAnimation from '../components/ProgressBar';
 
 const LoadingScreen = () => {
   // payment variables
-  let snapFee = 30000;
+  let snapFee = 35000;
   // 1 -> GoPay, 0 -> QRIS
   let paymentType = 0;  
 
