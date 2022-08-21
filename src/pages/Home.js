8e7465669c9ea2e8efd5946c2df10a81e069aa65
Gelar Pambudi <49099961+gelarpambudi@users.noreply.github.com>
2022-08-21 16:40:16 +0700

    Update Fee Homepage
    Update Fee Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
           </div>
         </Link>
         <h2 className='text-center mt-4'>
-         IDR 35.000
+         IDR 30.000
         </h2>
         <p className='text-center mb-0'>
           Having trouble with payment?
