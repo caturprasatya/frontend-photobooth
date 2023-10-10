9e598d7fe378b9c612d8e4061f44550e851403d4
caturprasatya <caturprasatya1224@gmail.com>
2023-10-11 05:42:33 +0700

    Merge: from other repository

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
-cd "C:\Users\Isama\Documents\Isa\Kerja\frontend-photobooth"
-call npm install --force
-call npm install -g serve --force
-call npm run build --force
+cd " C\:Users\user\Desktop\frontend-photobooth"
+call npm install
+call npm install -g serve
+call npm run build
 serve -s build
 pause
